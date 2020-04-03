e10ac7c62894583f2a1cffda304663b294ed9fa6
Jonathan Striebel <jonathan.striebel@gmail.com>
2020-04-03 18:05:59 +0200

    add tmnf

---
+++ tmnf/README.md
@@ -0,0 +1,22 @@
+# TrackMania Nations Forever Docker Image
+## :checkered_flag: :car: :whale:
+
+To use the image, download the following files into a seperate directory:
+* [run.sh](https://raw.githubusercontent.com/jstriebel/dockerfiles/master/tmnf/run.sh)
+* [docker-compose.yaml](https://raw.githubusercontent.com/jstriebel/dockerfiles/master/tmnf/docker-compose.yaml)
+* [tmnationsforever_setup.exe](http://trackmaniaforever.com/nations/)
+
+e.g. by running
+```
+mkdir tmnf
+cd tmnf
+wget https://raw.githubusercontent.com/jstriebel/dockerfiles/master/tmnf/run.sh
+wget https://raw.githubusercontent.com/jstriebel/dockerfiles/master/tmnf/docker-compose.yaml
+wget http://files.trackmaniaforever.com/tmnationsforever_setup.exe
+```
+
+Then simply call `./run.sh` and follow the installation instructions.
+By running `./run.sh` again, you can start the game, or open the settings with `./run.sh -s`.
+Use ``./run.sh -h` to see all options.
+
+Your game settings and executables will be stored in the `data` directory, and are still available when re-building or re-pulling the image.
---
+++ tmnf/run.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+scriptdir="$(dirname "$0")"
+cd "$scriptdir"
+
+if [ -d "data/.wine/drive_c/Program Files/TmNationsForever/" ]; then
+    EXTRA_ARGS="--user $(id -u):$(id -g)"
+    CMD=""
+    DISABLE_PULSE=false
+
+    if ! [ -z "${1-}" ]; then
+        if [ "$1" == "-b" ] || [ "$1" == "--build" ]; then
+            docker-compose build tmnf
+        elif [ "$1" == "-n" ] || [ "$1" == "--no-pulse" ]; then
+            DISABLE_PULSE=true
+        elif [ "$1" == "-p" ] || [ "$1" == "--pull" ]; then
+            docker-compose pull tmnf
+        elif [ "$1" == "-s" ] || [ "$1" == "--settings" ]; then
+            CMD="wine TmForeverLauncher.exe"
+        else
+            if ! [ "$1" == "-h" ] && ! [ "$1" == "--help" ]; then
+                echo "Got unrecognized option $1"
+            fi
+            echo "Usage: run.sh [OPTION]"
+            echo "Valid options are (only a single option is recognized):"
+            echo "  -b, --build       Rebuild docker image before starting the game."
+            echo "  -h, --help        Show this help message."
+            echo "  -n, --no-pulse    Disable pulse audio."
+            echo "  -p, --pull        Pull docker image before starting the game."
+            echo "  -s, --settings    Open game settings."
+            exit 0
+        fi
+    fi
+
+    if which pactl > /dev/null && ! $DISABLE_PULSE; then
+        echo "Using pulse audio."
+        PULSE_SOCKET="/tmp/pulseaudio.socket"
+        if ! [ -S "$PULSE_SOCKET" ]; then
+            pactl load-module module-native-protocol-unix socket="$PULSE_SOCKET"
+        fi
+        EXTRA_ARGS+=" -e PULSE_SERVER=unix:$PULSE_SOCKET"
+        EXTRA_ARGS+=" -e PULSE_COOKIE=/tmp/pulseaudio.cookie"
+        EXTRA_ARGS+=" -v $PULSE_SOCKET:$PULSE_SOCKET"
+    fi
+
+    docker-compose run --rm $EXTRA_ARGS tmnf $CMD
+    exit 0
+fi
+
+
+if [[ "$(docker images -q myimage:mytag 2> /dev/null)" == "" ]]; then
+    echo "Pulling Docker image…"
+    docker-compose pull tmnf
+fi
+
+echo "Installing TMNF …"
+
+if [ -f "tmnationsforever_setup.exe" ] ; then
+    echo -e "\e[32m✔\e[0m tmnationsforever_setup.exe"
+else
+    echo -e "\e[31mPlease download tmnationsforever_setup.exe from"
+    echo -e "http://trackmaniaforever.com/nations/"
+    echo -e "Aborting.\e[0m"
+    exit 1
+fi
+
+if [ -d "/tmp/.X11-unix" ]; then
+    echo -e "\e[32m✔\e[0m X11 socket"
+else
+    echo -e "\e[31mCan't find the X11 domain socket at /tmp/.X11-unix, aborting.\e[0m"
+    exit 1
+fi
+
+mkdir -p data
+
+echo "In the next step the TMNF installation GUI should open."
+echo "You need to use the preset default installation directory."
+read -p "Press enter to continue …"
+docker-compose run --rm -w /tmp tmnf wine tmnationsforever_setup.exe
+
+echo -e "\e[32mInstallation succesfull! Run again to start game.\e[0m"
